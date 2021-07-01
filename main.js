576a5dfeab748942e393beb76e0b652b1dec1165
GetThisDonut <thitisan_ch@elearning.cmu.ac.th>
2021-07-01 19:08:20 +0700

    mapping adding
    with :key

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ const app = Vue.createApp({
             in_stock:true,
             inventory:100,
             on_sale:true,
-            details:["50% cotton","30% wool","20% polyester"]
+            details:["50% cotton","30% wool","20% polyester"],
+            variants:[
+                {id:234,color:"green"},
+                {id:235, color:"red"}
+            ]
         }
     }
 });
